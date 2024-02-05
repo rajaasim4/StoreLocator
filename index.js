608048046266942780d9993ac26c8612f5c42832
Raja Asim <rajaasim4474@gmail.com>
2024-02-05 23:23:34 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+import storeList from "./Store.js";
+const map = L.map("map").setView([22.9074872, 79.07306671], 5);
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Coded by Raja Muhamad Asim with ❤️';
+const tileLayer = L.tileLayer(tileUrl, { attribution });
+tileLayer.addTo(map);
+
+//Generating List
+
+function generateList() {
+  const ul = document.querySelector(".list");
+  storeList.forEach((shop) => {
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    const a = document.createElement("a");
+    const p = document.createElement("p");
+    a.addEventListener("click", () => {
+      flyToStore(shop);
+    });
+    div.classList.add("shop-item");
+    a.innerText = shop.properties.name;
+    a.href = "#";
+    p.innerText = shop.properties.address;
+
+    div.appendChild(a);
+    div.appendChild(p);
+    li.appendChild(div);
+    ul.appendChild(li);
+  });
+}
+
+//Calling Generating the List
+
+generateList();
+
+function makePopupContent(shop) {
+  return `
+    <div>
+        <h4>${shop.properties.name}</h4>
+        <p>${shop.properties.address}</p>
+        <div class="phone-number">
+            <a href="tel:${shop.properties.phone}">${shop.properties.phone}</a>
+        </div>
+    </div>
+  `;
+}
+function onEachFeature(feature, layer) {
+  layer.bindPopup(makePopupContent(feature), {
+    closeButton: false,
+    offset: L.point(0, -8),
+  });
+}
+
+var myIcon = L.icon({
+  iconUrl: "./assets/pizza.png",
+  iconSize: [30, 40],
+});
+
+const shopsLayer = L.geoJSON(storeList, {
+  onEachFeature: onEachFeature,
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, { icon: myIcon });
+  },
+});
+shopsLayer.addTo(map);
+
+function flyToStore(store) {
+  const lat = store.geometry.coordinates[1];
+  const lng = store.geometry.coordinates[0];
+  map.flyTo([lat, lng], 14, {
+    duration: 3,
+  });
+  setTimeout(() => {
+    L.popup({ closeButton: false, offset: L.point(0, -8) })
+      .setLatLng([lat, lng])
+      .setContent(makePopupContent(store))
+      .openOn(map);
+  }, 3000);
+}
